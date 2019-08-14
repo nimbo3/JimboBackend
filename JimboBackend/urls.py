be7a0a0ec11698b7570b3a52fae621706b26d8f5
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-14 11:24:03 +0430

    add test for account

--- JimboBackend/urls.py
+++ JimboBackend/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 from SearchEngine import views
@@ -21,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('search/', views.search),
     path('', views.index),
+    url(r'^users/', include('accounts.urls')),
 ]
+
--- accounts/tests.py
+++ accounts/tests.py
@@ -32,3 +32,31 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.data['username'], data['username'])
         self.assertEqual(response.data['email'], data['email'])
         self.assertFalse('password' in response.data)
+
+    def test_create_user_with_short_password(self):
+        """
+        Ensure user is not created for password lengths less than 8.
+        """
+        data = {
+                'username': 'foobar',
+                'email': 'foobarbaz@example.com',
+                'password': 'foo'
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
+    def test_create_user_with_no_password(self):
+        data = {
+                'username': 'foobar',
+                'email': 'foobarbaz@example.com',
+                'password': ''
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
