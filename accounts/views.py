d0ef1efbe0d1744b7f706d1bb894feb0717a84d1
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-14 11:03:15 +0430

    create accounts test and views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class UserCreate(APIView):
+    """
+    Creates the user.
+    """
+
+    def post(self, request, format='json'):
+        return Response('hello')
