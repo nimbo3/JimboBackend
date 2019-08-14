c569af35ef60c3ecd4c0042c648d44d530124655
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-14 10:52:19 +0430

    add django restframework

--- SearchEngine/views.py
+++ SearchEngine/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
 import json
+
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 from elastic.views import search
 
@@ -8,9 +11,22 @@ def index(request):
     return HttpResponse(search(request.GET.get("q")), content_type="application/json")
 
 
-def login(request):
-    return None
+def login_view(request):
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    user = User.objects.filter(username=username)
 
 
-def register(request):
-    return None
+def register_view(request):
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    first_name = request.GET.get("first_name")
+    last_name = request.GET.get("last_name")
+    if username in None or password is None or first_name is None or last_name is None:
+        pass
+    user = User(username=username, first_name=first_name, last_name=last_name)
+    user.set_password(password)
+    user.save()
+    return HttpResponse(json.dumps({
+
+    }))
