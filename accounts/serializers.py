db11d3fc101254f31ed16a80f4c807b17b183133
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-14 11:27:51 +0430

    add test for account

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,17 +5,18 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-            required=True,
-            validators=[UniqueValidator(queryset=User.objects.all())]
-            )
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     username = serializers.CharField(
-            validators=[UniqueValidator(queryset=User.objects.all())]
-            )
+        max_length=32,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     password = serializers.CharField(min_length=8, write_only=True)
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'],
-             validated_data['password'])
+                                        validated_data['password'])
         return user
 
     class Meta:
