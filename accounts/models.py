7cdef147ec6c64fe86c752a0ef310a305cf1696c
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-18 18:15:26 +0430

    create search log models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Search(models.Model):
+
+    LANGUAGE_CHOICES = (
+        (0, ''),
+    )
+
+    CATEGORY_CHOICES = (
+        (0, 'Sport'),
+        (1, 'Sport'),
+        (2, 'Sport'),
+        (3, 'Sport'),
+        (4, 'Sport'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    query = models.CharField(max_length=512, null=False)
+    language = models.IntegerField(null=True)
+    category = models.IntegerField(null=True)
