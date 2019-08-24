bedae4428044f8502aaa8bdae484d9320e11c14f
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-24 08:09:49 +0430

    move search model from account to search engine

--- SearchEngine/models.py
+++ SearchEngine/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Search(models.Model):
+
+    LANGUAGE_CHOICES = (
+        (0, 'ar'),
+        (1, 'de'),
+        (2, 'en'),
+        (3, 'es'),
+        (4, 'fr'),
+        (5, 'fa'),
+        (6, 'ru'),
+    )
+
+    CATEGORY_CHOICES = (
+        (0, 'sport'),
+        (1, 'health'),
+        (2, 'news'),
+        (3, 'cat4'),
+        (4, 'cat5'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    query = models.CharField(max_length=512, null=False)
+    language = models.IntegerField(null=True, choices=LANGUAGE_CHOICES)
+    category = models.IntegerField(null=True, choices=CATEGORY_CHOICES)
