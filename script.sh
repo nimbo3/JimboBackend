5b213ff1da4d3a1ebf21b2f009101d717df210c8
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-25 08:54:04 +0430

    create run script

--- script.sh
+++ script.sh
@@ -1,3 +1,20 @@
 read -p 'enter username: ' username
 read -p 'enter server address: ' address
 read -p 'enter server ssh port: ' port
+echo connecting to server:
+ssh $username@$address -p $port << EOF
+  cd ~/website/
+  source venv/bin/activate
+  screen -X -S Joojle quit
+  cd backend/
+  git stash
+  git pull origin master
+  python3 manage.py makemigrations
+  python3 manage.py migrate
+  cd ../frontend
+  git stash
+  git pull origin master
+  cd ..
+  screen -dmS -S Joojle sudo docker-compose up
+  logout
+EOF
