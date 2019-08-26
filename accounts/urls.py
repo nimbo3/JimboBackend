4f5810cb82426b4088cd9bfbeaadb19d22aadfae
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-26 20:11:08 +0430

    ready for publish

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     url('account-create', views.UserCreate.as_view(), name='account-create'),
     url('account-login', views.UserLogin.as_view(), name='account-login'),
-    # url(r'^api/v1/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
